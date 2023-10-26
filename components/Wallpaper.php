8da4d6dade70f8e27a5a10c0bf1850ce691f81b0
NielsvandenDries <niels@openmindedit.nl>
2023-10-26 11:47:43 +0200

    update

--- components/Wallpaper.php
+++ components/Wallpaper.php
@@ -18,13 +18,13 @@ class Wallpaper extends ComponentBase
         return [
             'unsplashAccessKey' => [
                 'title' => 'Unsplash Access Key',
-                'description' => 'Voer je Unsplash Access Key in',
+                'description' => 'Enter your Unsplash Access Key',
                 'default' => '',
                 'type' => 'string',
             ],
             'unsplashSecretKey' => [
                 'title' => 'Unsplash Secret Key',
-                'description' => 'Voer je Unsplash Secret Key in',
+                'description' => 'Enter your Unsplash Secret Key',
                 'default' => '',
                 'type' => 'string',
             ],
@@ -33,17 +33,13 @@ class Wallpaper extends ComponentBase
 
     public function onRun()
     {
-        // Haal de Unsplash API Access Key en Secret Key op uit de componentinstellingen
         $accessKey = $this->property('unsplashAccessKey');
         $secretKey = $this->property('unsplashSecretKey');
 
-        // Stel de basis-URL in voor de Unsplash API
         $apiUrl = 'https://api.unsplash.com/photos/random';
 
-        // Maak een HTTP-client
         $client = new Client();
 
-        // Genereer een OAuth2-bearer token met je Access Key en Secret Key
         $response = $client->post('https://unsplash.com/oauth/token', [
             'form_params' => [
                 'client_id' => $accessKey,
@@ -55,23 +51,17 @@ class Wallpaper extends ComponentBase
         $tokenData = json_decode($response->getBody(), true);
         $accessToken = $tokenData['access_token'];
 
-        // Doe een GET-verzoek naar de API met het verkregen bearer token
         $response = $client->get($apiUrl, [
             'headers' => [
                 'Authorization' => 'Bearer ' . $accessToken,
             ],
             'query' => [
-                'count' => 1, // Aantal wallpapers dat je wilt ophalen
+                'count' => 1,
             ]
         ]);
 
-        // Verwerk de API-respons
         $data = json_decode($response->getBody(), true);
 
-        // Nu kun je de gegevens gebruiken zoals je wilt
-        // $data bevat informatie over de opgehaalde wallpaper, zoals URL en metadata
-
-        // Stel de gegevens in zodat ze beschikbaar zijn in de component's view
         $this->page['wallpaperData'] = $data;
     }
 }
